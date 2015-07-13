ce3f5b52c5759dff4f3f18766171cd17e7dc6fcd
fomy <fumin@hust.edu.cn>
2015-07-13 11:46:06 +0800

    idetical files analysis

--- file_identical_check.py
+++ file_identical_check.py
@@ -9,26 +9,66 @@ def check_one_file(one, others):
 
     return False
 
-
-def check_identical_files(files):
-    same_files = 0
-    total_files = 0
-    diff = []
+def check_identical_file_names(trace):
+    logical_files = 0
+    physical_files = 0
+    total_names = 0
+    total_suffix = 0
     while True:
-        if check_one_file(files[0], files[1:]):
-            same_files += 1
-        else:
-            diff.append(files[0])
+        line = list(itertools.islice(trace, 1))
+        if len(line) == 0:
+            break;
+        fnum = int(line[0].split()[1])
+        physical_files += 1
+        logical_files += fnum
+        iflines = list(itertools.islice(trace, fnum))
 
-        total_files += 1
-        files.remove(files[0])
-        if len(files) == 1:
-            break
-    
-    if len(diff) > 0:
-        diff.append(files[0])
+        names = [ifl.split()[3] for ifl in iflines]
+        sufs = [ifl.split()[-1] for ifl in iflines]
 
-    return total_files, same_files, diff
+        nameset = {}
+        for name in names:
+            if name in nameset:
+                nameset[name] += 1
+            else:
+                nameset[name] = 1
+
+        suffixset = {}
+        for suf in sufs:
+            if suf in suffixset:
+                suffixset[suf] += 1
+            else:
+                suffixset[suf] = 1
+
+        total_names += len(nameset)
+        total_suffix += len(suffixset)
+
+    print >>sys.stderr, logical_files, physical_files, total_names, total_suffix
+
+def get_popular_types(trace):
+    suffixset = {}
+    while True:
+        line = list(itertools.islice(trace, 1))
+        if len(line) == 0:
+            break;
+        fnum = int(line[0].split()[1])
+        iflines = list(itertools.islice(trace, fnum))
+
+        files = [ifl.split() for ifl in iflines]
+        for file in files: 
+            if file[-1] in suffixset:
+                suffixset[file[-1]] += int(file[2])
+            else:
+                suffixset[file[-1]] = int(file[2])
+
+    top_suffix = []
+    for suf in suffixset:
+        top_suffix.append((suffixset[suf], suf))
+        top_suffix = sorted(top_suffix, reverse=True)
+        if(len(top_suffix) > 10):
+            top_suffix.pop()
+
+    print top_suffix
 
 
 # find all identical files
@@ -37,27 +77,26 @@ def check_identical_files(files):
 # -o output all suffix
 if __name__ == "__main__":
 
+    (opts, args) = getopt.getopt(sys.argv[1:], "no", ["cluster"])
+
     col = -1
-    file = open(sys.argv[1], "r")
+    task = "check names"
+    for o, a in opts:
+        if o in ["-n"]:
+            col = 3
+            task = "check names"
+        elif o in ["-s"]:
+            col = -1
+            task = "check suffix"
+        elif o in ["o"]:
+            col = -1
+            task = "popular suffix"
 
-    total_names = 0;
-    same_names = 0;
-    while True:
-        line = list(itertools.islice(file, 1))
-        if len(line) == 0:
-            break;
-        fnum = line[0].split()[1]
-        iflines = list(itertools.islice(file, int(fnum)))
+    trace = open(args[0], "r")
 
-        (t,s,d) = check_identical_files([ifl.split()[col] for ifl in iflines])
+    if task == "check name":
+        check_identical_file_names(trace)
+    else:
+        get_popular_types(trace)
 
-        if t != s:
-            #print [ifl.split()[col] for ifl in iflines]
-            print d
-        else:
-            assert(len(d) == 0)
-
-        total_names += t
-        same_names += s
-
-    print >>sys.stderr, total_names, same_names
+    trace.close()
