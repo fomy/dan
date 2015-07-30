8954245af4b3f999b4f6c8f7f522488cb7124504
fomy <fumin@hust.edu.cn>
2015-07-30 17:45:34 +0800

    fix a bug in simd exporter

--- simd_exporter.c
+++ simd_exporter.c
@@ -5,8 +5,7 @@
 #include <inttypes.h>
 #include <getopt.h>
 #include "store.h"
-
-void output_simd_trace(int dedup){
+void output_nodedup_simd_trace(){
     init_iterator("CHUNK");
 
     struct chunk_rec r;
@@ -15,7 +14,48 @@ void output_simd_trace(int dedup){
     int64_t physical_size = 0;
     int64_t logical_size = 0;
 
-    int64_t sum = 0;
+    double sum = 0;
+    int64_t count = 0;
+    while(iterate_chunk(&r, 0) == 0){
+
+        physical_size += r.csize;
+        int64_t rc = r.rcount;
+        logical_size += r.csize * rc;
+
+        struct file_rec file;
+        memset(&file, 0, sizeof(file));
+        int i = 0;
+        int64_t size = 0;
+        for(; i < r.rcount; i++){
+            file.fid = r.list[r.rcount + i];
+            search_file(&file);
+            size += file.fsize;
+            count++;
+            sum += 1.0*file.fsize/1024/1024;
+            printf("%"PRId64"\n", file.fsize);
+        }
+    }
+
+    /* The last number if the deduplication ratio */
+    printf("%.1f\n", 1.0);
+
+    fprintf(stderr, "Physical Size = %"PRId64", Logical Size = %"PRId64", D/R = %.4f\n", physical_size, logical_size, 1.0*logical_size/physical_size);
+    fprintf(stderr, "Sum = %"PRId64", Count = %"PRId64", Avg. = %.4f MB\n", sum, count, sum/count);
+
+    close_iterator();
+
+}
+
+void output_dedup_simd_trace(){
+    init_iterator("CHUNK");
+
+    struct chunk_rec r;
+    memset(&r, 0, sizeof(r));
+
+    int64_t physical_size = 0;
+    int64_t logical_size = 0;
+
+    double sum = 0;
     int64_t count = 0;
     while(iterate_chunk(&r, 1) == 0){
 
@@ -31,27 +71,17 @@ void output_simd_trace(int dedup){
             file.fid = r.list[r.rcount + i];
             search_file(&file);
             size += file.fsize;
-            if(!dedup){
-                count++;
-                sum += file.fsize;
-                printf("%"PRId64"\n", file.fsize);
-            }
         }
-        if(dedup){
-            count++;
-            sum += size;
-            printf("%"PRId64"\n", size);
-        }
+        count++;
+        sum += 1.0*size/1024/1024;
+        printf("%"PRId64"\n", size);
     }
 
     /* The last number if the deduplication ratio */
-    if(dedup)
-        printf("%.4f\n", 1.0*logical_size/physical_size);
-    else
-        printf("%.1f\n", 1.0);
+    printf("%.4f\n", 1.0*logical_size/physical_size);
 
     fprintf(stderr, "Physical Size = %"PRId64", Logical Size = %"PRId64", D/R = %.4f\n", physical_size, logical_size, 1.0*logical_size/physical_size);
-    fprintf(stderr, "Sum = %"PRId64", Count = %"PRId64", Avg. = %.2f\n", sum, count, 1.0*sum/count);
+    fprintf(stderr, "Sum = %"PRId64", Count = %"PRId64", Avg. = %.4f MB\n", sum, count, sum/count);
 
     close_iterator();
 
@@ -76,7 +106,10 @@ int main(int argc, char *argv[])
 
     open_database();
 
-    output_simd_trace(dedup);
+    if(dedup)
+        output_dedup_simd_trace();
+    else
+        output_nodedup_simd_trace();
 
     close_database();
 
